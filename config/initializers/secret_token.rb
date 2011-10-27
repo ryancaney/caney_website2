61b377e224c17fcac24d134240fe5aca9ef28bfe
ryancaney <ryan.caney@sv.cmu.edu>
2011-10-27 00:05:51 -0400

    added gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaneyWebsite2::Application.config.secret_token = '7d38147772deab7876d4acb034d188d6100ecd803c2dfab5ecea4875b346f47cc277923d61ad9dc2040fb9280ac2b530713f8674948f00c77cfeaf9567f7e898'
